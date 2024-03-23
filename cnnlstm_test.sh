f5e34b0d1d5d0fa64d21a64a59363085e85bd2e3
Anush Veeranala <jun@member.fsf.org>
2024-03-23 10:12:52 -0400

    Updated CNN LSTM Model, base paths

--- cnnlstm_test.sh
+++ cnnlstm_test.sh
@@ -9,7 +9,8 @@ module load academic-ml/spring-2024
 conda activate spring-2024-pyt
 
 # Change this path to point to your project directory
-export PYTHONPATH="/projectnb/ds598/admin/tgardos/sp2024_midterm:$PYTHONPATH"
+export PYTHONPATH="/projectnb/ds598/students/jun/vizwiz-image-captioning:$PYTHONPATH" # Set this!!!
+# export PYTHONPATH="/projectnb/ds598/admin/tgardos/sp2024_midterm:$PYTHONPATH"
 
 #python -m spacy download en_core_web_sm   # download spacy model
 python src/cnn_lstm/test.py
