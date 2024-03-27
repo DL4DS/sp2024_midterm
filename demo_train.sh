fd44e77eebe892937c837b008d443cdbeee5695f
xinyihu3721 <xhu07@bu.edu>
2024-03-27 16:03:39 -0400

    commit

--- demo_train.sh
+++ demo_train.sh
@@ -2,6 +2,9 @@
 
 # Set SCC project
 #$ -P ds598
+#$ -l h_rt=24:00:00 
+#$ -m beas
+#$ -M xhu07@bu.edu
 
 module load miniconda
 module load academic-ml/spring-2024
@@ -9,9 +12,11 @@ module load academic-ml/spring-2024
 conda activate spring-2024-pyt
 
 # Change this path to point to your project directory
-export PYTHONPATH="/projectnb/ds598/admin/tgardos/sp2024_midterm:$PYTHONPATH"
+export PYTHONPATH="/projectnb/ds598/students/xhu07/sp2024_midterm:$PYTHONPATH"
 
-python src/demo_model/train.py
+python3 src/demo_model/train.py
 
 ### The command below is used to submit the job to the cluster
 ### qsub -pe omp 4 -P ds598 -l gpus=1 demo_train.sh
+
+### remeber to change dataloader size
