1fe898dca275fb05cc9702163458f0b625e67bf9
Weining Mai <84426364+weibb123@users.noreply.github.com>
2024-03-27 18:41:20 +0000

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# Set SCC project
+#$ -P ds598
+
+module load miniconda
+module load academic-ml/spring-2024
+conda activate spring-2024-pyt
+
+# Change this path to point to your project directory
+export PYTHONPATH="/projectnb/ds598/students/weimai/sp2024_midterm:$PYTHONPATH"
+
+python -m spacy download en_core_web_sm 
+python src/demo_model/test.py
+
+### The command below is used to submit the job to the cluster
+### qsub -pe omp 4 -P ds598 -l gpus=1 test.sh
