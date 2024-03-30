f02b24ddf8039ecee23689564797d3af736b39c8
Nikh1ta <nikhitamantravadi@gmail.com>
2024-03-30 10:04:51 -0400

    commit

--- demo_train.sh
+++ demo_train.sh
@@ -11,9 +11,10 @@ conda activate spring-2024-pyt
 # Change this path to point to your project directory
 export PYTHONPATH="/projectnb/ds598/students/nikhitam/sp2024_midterm:$PYTHONPATH"
 
-python /projectnb/ds598/students/nikhitam/sp2024_midterm/
+python /projectnb/ds598/students/nikhitam/sp2024_midterm/src/demo_model/train.py
+
 
 
 ### The command below is used to submit the job to the cluster
-qsub -pe omp 4 -P ds598 -l gpus=1 demo_train.sh
+#qsub -pe omp 4 -P ds598 -l gpus=1 demo_train.sh
 
