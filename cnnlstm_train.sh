8b269a111f76d514275b49431773e77480c8168b
Nikh1ta <nikhitamantravadi@gmail.com>
2024-03-30 10:07:14 -0400

    commit

--- cnnlstm_train.sh
+++ cnnlstm_train.sh
@@ -12,7 +12,7 @@ conda activate spring-2024-pyt
 export PYTHONPATH="/projectnb/ds598/admin/tgardos/sp2024_midterm:$PYTHONPATH" # Set this!!!
 
 python -m spacy download en_core_web_sm   # download spacy model
-python /projectnb/ds598/students/nikhitam/sp2024_midterm/src/cnn_lstm/test.py
+python /projectnb/ds598/students/nikhitam/sp2024_midterm/src/demo_model/train.py
 
 
 ### The command below is used to submit the job to the cluster
