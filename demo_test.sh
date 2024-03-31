da2ede9dee1b3c1a8f07bffe9510ce1fe503efc6
Jack Campbell <jawicamp@scc1.bu.edu>
2024-03-31 12:39:38 -0400

    updated changes with good CIDER score

--- demo_test.sh
+++ demo_test.sh
@@ -2,6 +2,9 @@
 
 # Set SCC project
 #$ -P ds598
+#$ -l h_rt=30:00:00
+#$ -m beas
+#$ -M jawicamp@bu.edu
 
 module load miniconda
 module load academic-ml/spring-2024
@@ -9,9 +12,10 @@ module load academic-ml/spring-2024
 conda activate spring-2024-pyt
 
 # Change this path to point to your project directory
-export PYTHONPATH="/projectnb/ds598/admin/tgardos/sp2024_midterm:$PYTHONPATH" # Set this!!!
+export PYTHONPATH="/projectnb/ds598/students/jawicamp/sp2024_midterm:$PYTHONPATH" # Set this!!!
 
+python -m spacy download en_core_web_sm   # download spacy model
 python src/demo_model/test.py
 
 ### The command below is used to submit the job to the cluster
-### qsub -pe omp 4 -P ds598 -l gpus=1 git_test.sh
+### qsub -pe omp 4 -P ds598 -l gpus=2 demo_test.sh
