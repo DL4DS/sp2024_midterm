6306e0e07d8e077d201e8a8a21488e863f6cb11f
Carmen Pelayo Fernandez <carmenpelayofdez@gmail.com>
2024-04-02 14:13:09 -0400

    update midterm files

--- src/demo_model/train.py
+++ src/demo_model/train.py
@@ -8,6 +8,8 @@ from dataset import DemoDataset   ## This is a local import from dataset.pyA
 from tqdm import tqdm
 from transformers import AutoProcessor
 from transformers import AutoModelForCausalLM
+from transformers import BlipProcessor, BlipForConditionalGeneration #ADDED BY CARMEN
+#from transformers import AutoProcessor, AutoModelForSeq2SeqLM #ADDED BY CARMEN
 from PIL import Image
 import matplotlib.pyplot as plt
 import os
@@ -32,7 +34,9 @@ create_directory(DEMO_SAVE_PATH + "/examples")
 # to encode and decode text and images.
 # https://huggingface.co/docs/transformers/model_doc/auto#transformers.AutoProcessor
 try:
-    processor = AutoProcessor.from_pretrained("replace-with-model-choice", cache_dir=CACHE_DIR)
+    #processor = AutoProcessor.from_pretrained("MarianMTModel", cache_dir=CACHE_DIR)
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=CACHE_DIR)
+    #processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=CACHE_DIR) #ADDED BY CARMEN
 except Exception as e:
     print("You need to pick a pre-trained model from HuggingFace.")
     print("Exception: ", e)
@@ -51,8 +55,8 @@ val_dataset = DemoDataset(
 )
 
 ### Use the Subset while debugging ###
-# train_dataset = Subset(train_dataset, range(100))
-# val_dataset = Subset(val_dataset, range(10))
+#train_dataset = Subset(train_dataset, range(10))
+#val_dataset = Subset(val_dataset, range(10))
 
 ### Since, subset is used above, the dataset object needs to be called with a .dataset, to access the original dataset. So while using the full dataset, the below is done. ###
 train_dataset = Subset(train_dataset, range(len(train_dataset)))
@@ -64,6 +68,7 @@ print(f"LEN VAL IMAGE IDS: {len(val_dataset.dataset.image_ids)}")
 print("SANITY CHECK DONE!!")
 
 
+
 train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=8)
 val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=32)
 
@@ -72,15 +77,18 @@ val_dataloader = DataLoader(val_dataset, shuffle=False, batch_size=32)
 # model you want to fine-tune. This will allow you to use the model to train and evaluate
 # on the VizWiz dataset.
 try:
-    model = AutoModelForCausalLM.from_pretrained("replace-with-model-choice", cache_dir=CACHE_DIR)
+    #model = AutoModelForCausalLM.from_pretrained("MarianConfig", cache_dir=CACHE_DIR)
+    #model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=CACHE_DIR) #ADDED BY CARMEN
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=CACHE_DIR)
 except Exception as e:
     print("You need to pick a pre-trained model from HuggingFace.")
     print("Exception: ", e)
 
 ## TODO Select your model optimizer
 try:
-    raise NotImplementedError("Select your model optimizer")
-    optimizer = None   # pick one from torch.optim
+    #raise NotImplementedError("Select your model optimizer")
+    #optimizer = None   # pick one from torch.optim
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5) #ADDED BY CARMEN
 except Exception as e:
     print("You need to pick an optimizer from torch.optim.")
     print("Exception: ", e)
@@ -102,12 +110,13 @@ def train(loger, train_dataloader, model, optimizer, device, processor):
 
     for idx, batch in progress_bar:
         input_ids = batch.pop("input_ids").to(device)
+        attention_mask = batch.pop("attention_mask").to(device) #CARMEN
         pixel_values = batch.pop("pixel_values").to(device)
 
         optimizer.zero_grad()
 
         outputs = model(
-            input_ids=input_ids, pixel_values=pixel_values, labels=input_ids
+            input_ids=input_ids, attention_mask = attention_mask, pixel_values=pixel_values, labels=input_ids
         )
 
         loss = outputs.loss
@@ -134,7 +143,12 @@ def evaluate(
         pixel_values = batch.pop("pixel_values").to(device)
 
         with torch.no_grad():
-            outputs = model.generate(pixel_values=pixel_values, max_length=50)
+            #outputs = model.generate(pixel_values=pixel_values, max_length=50)
+            if torch.cuda.device_count() > 1:
+                outputs = model.module.generate(pixel_values=pixel_values, max_length=50)
+            else:
+                outputs = model.generate(pixel_values=pixel_values, max_length=50)
+
 
         # Decode the generated ids to text
         generated_captions = processor.batch_decode(outputs, skip_special_tokens=True)
@@ -223,7 +237,7 @@ def get_val_examples(vizwizEval, vizwizRes, plot_captions_dict, epoch, method="C
 
 
 best_score = 0
-for epoch in range(3):
+for epoch in range(10):
     print(f"Epoch: {epoch+1}")
     # Wrap the dataloader with tqdm for a progress bar
     progress_bar = tqdm(
@@ -235,7 +249,7 @@ for epoch in range(3):
     logger.info(f"Loss at epoch {epoch}: {loss}")
 
     # Evaluate the model every 3 epochs
-    if epoch % 3 == 0:
+    if epoch % 1 == 0:
         vizwizEval, vizwizRes, plot_captions_dict = evaluate(
             logger,
             epoch,
@@ -249,7 +263,10 @@ for epoch in range(3):
         score = vizwizEval.eval[method]
         if score > best_score:
             best_score = score
-            model.save_pretrained(f"{DEMO_SAVE_PATH}/best_model")
+            if torch.cuda.device_count() > 1:
+                model.module.save_pretrained(f"{DEMO_SAVE_PATH}/best_model")
+            else:
+                model.save_pretrained(f"{DEMO_SAVE_PATH}/best_model")
             logger.info(f"New best score: {best_score}. Model saved")
 
         get_val_examples(vizwizEval, vizwizRes, plot_captions_dict, epoch, method)
