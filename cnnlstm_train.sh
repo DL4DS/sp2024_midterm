1d0d7d17f8488f2af11c7ce4b5c5efdd03b390e5
lilinSTART <144761144+lilinSTART@users.noreply.github.com>
2024-04-06 17:53:00 -0400

    Update cnnlstm_train.sh

--- cnnlstm_train.sh
+++ cnnlstm_train.sh
@@ -11,7 +11,7 @@ conda activate spring-2024-pyt
 # Change this path to point to your project directory
 PYTHONPATH="/projectnb/ds598/students/lilinj/sp2024_midterm:$PYTHONPATH" # Set this!!!
 
-python -m spacy download en_core_web_sm   # download spacy model
+#python -m spacy download en_core_web_sm   # download spacy model
 python src/cnn_lstm/train.py
 
 ### The command below is used to submit the job to the cluster
